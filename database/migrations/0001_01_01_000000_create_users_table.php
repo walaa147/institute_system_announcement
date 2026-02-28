b1f5aeed7673fa1b093dfbd5ed8a0aad0d967097
halauser <ha00la2002@gmail.com>
2026-02-28 06:19:48 +0300

    add extra vars for institute & user Table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('institute_id')->nullable()->constrained('institutes')->nullOnDelete();
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- database/migrations/2025_12_15_035503_create_institutes_table.php
+++ database/migrations/2025_12_15_035503_create_institutes_table.php
@@ -17,13 +17,29 @@ return new class extends Migration
         $table->string('name_en', 255)->nullable(); // اسم المعهد بالانجليزي
         $table->text('description')->nullable();
         $table->string('address', 255)->nullable(); // تم تغييره لـ string لأنه أنسب للعنوان
-        $table->string('photo_path', 2048)->nullable(); // مسار الصورة يفضل أن يكون أطول قليلاً
+        //$table->string('photo_path', 2048)->nullable(); // مسار الصورة يفضل أن يكون أطول قليلاً
 
         // إضافات اختيارية مهمة للمعهد
         $table->string('phone', 20)->nullable(); // رقم التواصل
         $table->string('email', 100)->nullable(); // ايميل المعهد الرسمي
 
+
+        $table->string('website')->nullable();
+        $table->string('slug')->unique();
+        $table->decimal('lat', 10, 8)->nullable();
+        $table->decimal('lng', 11, 8)->nullable();
+        $table->string('logo',2048)->nullable();
+        $table->string('cover_photo' , 2048)->nullable();
+
+        $table->decimal('commission_rate', 5, 2)->default(0);
+        $table->integer('priority_level')->default(0);
+        $table->integer('points_balance')->default(0);
+        $table->boolean('status')->default(true);
+
+
         $table->timestamps();
+        $table->softDeletes();
+
     });
 }
 
